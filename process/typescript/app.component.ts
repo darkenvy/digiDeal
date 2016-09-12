6292241f0b1847fdddec70d8d7ffeae5c57e86e9
darkenvy <darkenvy6@gmail.com>
2016-09-12 13:09:19 -0700

    Got the API AJAX call workinggit add -A

--- process/typescript/app.component.ts
+++ process/typescript/app.component.ts
@@ -1,7 +1,35 @@
-import {Component} from 'angular2/core';
+import {Component, Inject} from 'angular2/core';
+import { Http ,Headers , HTTP_PROVIDERS } from 'angular2/http';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'anydeal-rss',
-  template: '<b>YAY</b>'
+  template: '<b>YAY</b>',
+  viewProviders: [HTTP_PROVIDERS]
+  // providers: [HTTP_PROVIDERS]
 })
-export class AppComponent {}
+export class AppComponent {
+  http: Http;
+  people;
+  constructor(http: Http) {
+    http.get('https://swapi.co/api/people/1/?format=json')
+      // Call map on the response observable to get the parsed people object
+      .map(res => res.json())
+      // Subscribe to the observable to get the parsed people object and attach it to the
+      // component
+      // .subscribe(people => this.people = people);
+      .subscribe(people => console.log(people));
+  }
+}
+
+
+
+// import {Http, HTTP_PROVIDERS} from 'angular2/http';
+// @Component({
+  // selector: 'http-app',
+  // viewProviders: [HTTP_PROVIDERS],
+  // templateUrl: 'people.html'
+// })
+// class PeopleComponent {
+  
+// }
